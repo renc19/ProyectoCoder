7e0f6eef78f5e08c479f2fbdb1edd632352f9eac
renc19 <rennci1.comp@gmail.com>
2023-09-28 08:50:45 -0300

    Punto seguro. Models listos.

---
+++ AppCoder/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
+
+# Create your views here.
+def curso(self):
+    curso = Curso(nombre="Desarrollo web", camada="19881")
+    curso.save()
+    documentoDeTexto = f"--->Curso: {curso.nombre}      Camada: {curso.camada}"
+
+    return HttpResponse(documentoDeTexto)
